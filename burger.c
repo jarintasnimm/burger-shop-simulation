d661948ca31ace1e29d02756817e00b168ce1165
jarintasnimm <jarint178@gmail.com>
2025-07-17 15:49:53 +0600

    Update burger.c

--- burger.c
+++ burger.c
@@ -92,12 +92,13 @@ void *cashier_thread(void *arg) {
     return NULL;
 }
 
+
 void *customer_thread(void *arg) {
     thread_arg_t *data = (thread_arg_t *)arg;
     int id = data->id;
     printf("Customer %d entered\n", id);
     sem_post(data->init_done);
-    sleep(rand() % WAIT_TIME + 1);
+    sleep(rand() % WAIT_TIME + 1);  // Simulate arrival time
 
     sem_wait(&customer_mutex);
     sem_post(&customer_sem);
@@ -107,11 +108,14 @@ void *customer_thread(void *arg) {
     sem_post(&customer_mutex);
 
     printf("Customer %d ordering from Cashier %d\n", id, c.id);
-    sem_post(c.order);
-    sem_wait(c.food_ready);
+
+    sem_post(c.order);              // Place order
+    sem_wait(c.food_ready);         // Wait for food
     printf("Customer %d received food from Cashier %d\n", id, c.id);
+
     return NULL;
 }
+
 
 int main() {
     srand(time(NULL));
